a33d0b46c073b00a151619693c3345b719ec873a
Seweryn <seweryn.kaminski@gmail.com>
2021-04-11 10:05:43 +0200

    Update main.cpp
    Added some comments and randomize execution

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <chrono>
 #include <defer>
 
 using namespace std;
+using namespace std::chrono;
 
 void defer_test ( ) {
     cout << "defer_test begin\n";
-    int* p=new int();
+    int* p=new int();   // dynamic memory allocation
 
-    defer{
+    defer{              // this is not executed here!
         delete p;
         cout << "defer executed\n";
     };
     
-    if ( !random() ) {
+    auto rnd = duration_cast<milliseconds>(system_clock::now().time_since_epoch>
+    if ( rnd & 1 ) {    // 50% of probability (try execute few times to see path A and B)
         cout << "defer_test end A\n";
-        return;
+        return;         // triggers defer section
     }
 
     *p=10;
     cout << "defer_test end B\n";
+                        // triggers defer section
 }
 
 int main ( ) {
